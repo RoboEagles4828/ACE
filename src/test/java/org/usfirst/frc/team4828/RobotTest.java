ca89066dbc8207313eb731bf0603eb216ba11076
Jackie Chen <Jackie Chen>
2017-01-12 20:26:19 -0500

    Add JUnit Testing Stub
    Added an empty JUnit folder, including dependencies on the build.gradle.

---
+++ src/test/java/org/usfirst/frc/team4828/RobotTest.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team4828;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by cheez on 1/12/2017.
+ */
+public class RobotTest {
+    @Test
+    public void testSomething() {
+        Assert.assertTrue(true);
+    }
+}
