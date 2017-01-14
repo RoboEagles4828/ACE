8d6ac424a5a18a369ec3f219820ec852e5f97902
Jackie Chen <cheezballz3.14@gmail.com>
2017-01-14 12:41:05 -0500

    Add Unit Testing Stub

---
+++ src/test/java/org/usfirst/frc/team4828/RobotTest.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4828;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RobotTest {
+    @Test
+    public void testTheThings() {
+        Assert.assertTrue(true);
+    }
+}
