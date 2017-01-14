5e47a456c44726cadf0cc7b0d959e04ddd0618cd
Jackie Chen <cheezballz3.14@gmail.com>
2017-01-14 18:18:33 -0500

    Add another test

--- src/test/java/org/usfirst/frc/team4828/RobotTest.java
+++ src/test/java/org/usfirst/frc/team4828/RobotTest.java
@@ -9,4 +9,8 @@ public class RobotTest {
         System.out.println("Test ran!");
         Assert.assertTrue(true);
     }
+    @Test
+    public void anotherTest() {
+        Assert.assertEquals("Test that 1 = 1",1,1);
+    }
 }
