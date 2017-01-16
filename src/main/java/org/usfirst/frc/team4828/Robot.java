2cc89f3905cc35d575dc44740655e1cf0ff85d57
Jackie Chen <cheezballz3.14@gmail.com>
2017-01-16 13:56:31 -0500

    New Version Merge (#8)
    * Add Unit Testing Stub
    * Add .travis.yml
    * Fix not running tests
    use gradlew test --rerun-tasks to run tests multiple times
    * Add mecanum test code
    * Add executable permissions for gradlew
    * Change position of chmod
    * variable name change
    * Add another test
    * Update for GradleRIO 2017.1.0
    * Update gradle version
    * Reformat readme
    * Fix readme
    * Update build.gradle for JUnit
    * Just a test
    * Fixing formatting
    * formatting fixed now for sure

--- src/main/java/org/usfirst/frc/team4828/Robot.java
+++ src/main/java/org/usfirst/frc/team4828/Robot.java
@@ -1,17 +1,25 @@
 package org.usfirst.frc.team4828;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+
 
 public class Robot extends IterativeRobot {
+    Joystick driveStick;
+    RobotDrive drive;
+
     @Override
     public void robotInit() {
         super.robotInit();
         System.out.println("THE ROBOT TURNED ON");
+        driveStick = new Joystick(0);
+        drive = new RobotDrive(1, 2, 3, 4);
     }
 
     @Override
     public void teleopPeriodic() {
         super.teleopPeriodic();
-        System.out.println("Teleop running!");
+        drive.mecanumDrive_Cartesian(driveStick.getX(), driveStick.getY(), driveStick.getTwist(), 0);
     }
 }
---
+++ src/test/java/org/usfirst/frc/team4828/RobotTest.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team4828;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RobotTest {
+    @Test
+    public void testTheThings() {
+        System.out.println("Test ran!");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void anotherTest() {
+        Assert.assertEquals("Test that 1 = 1",1,1);
+    }
+}
