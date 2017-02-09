512180c8fc18a92c31d64d1ee7053d7e6d449244
P1n3appl3 <josephryan3.14@gmail.com>
2017-02-09 18:40:43 -0500

    Fix CodeClimate Issues

--- src/test/java/org/usfirst/frc/team4828/DriveTrainTest.java
+++ src/test/java/org/usfirst/frc/team4828/DriveTrainTest.java
@@ -1,25 +1,33 @@
 package org.usfirst.frc.team4828;
 
+import java.util.Random;
 import org.junit.Assert;
 import org.junit.Test;
-import java.util.Random;
 
 public class DriveTrainTest {
     private static final int TEST_CASES = 420420;
 
     @Test
     public void testNormalize() {
-        Random r = new Random();
-        DriveTrain d = new DriveTrain();
+        Random rand = new Random();
+        DriveTrain drive = new DriveTrain();
         for(int j = 1; j < TEST_CASES + 1; j++) {
             double[] testData = new double[4];
             for (int i = 0; i < 4; i++) {
-                testData[i] = r.nextDouble() * 2;
+                testData[i] = rand.nextDouble() * 2;
             }
-            d.normalize(testData);
+            drive.normalize(testData);
             for (int i = 0; i < 4; i++) {
                 if (testData[i] > 1.0) {
-                    System.out.println("Normalization failed: " + testData[i] + " > 1.0 (" + j + " of " + TEST_CASES + ")");
+                    System.out.println(
+                            "Normalization failed: "
+                                    + testData[i]
+                                    + " > 1.0 ("
+                                    + j
+                                    + " of "
+                                    + TEST_CASES
+                                    + ")"
+                    );
                     Assert.fail();
                 }
             }
