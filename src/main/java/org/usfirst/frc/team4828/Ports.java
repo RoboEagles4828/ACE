b9a7bb5172fa5d2503cc83961c79129b38ed21aa
Jackie Chen <cheezballz3.14@gmail.com>
2017-02-11 12:22:12 -0500

    Implement Ports.java

--- src/main/java/org/usfirst/frc/team4828/Ports.java
+++ src/main/java/org/usfirst/frc/team4828/Ports.java
@@ -4,15 +4,28 @@ package org.usfirst.frc.team4828;
  * Created by Jackie on 2/11/2017.
  */
 public class Ports {
+    private Ports(){
+        throw new AssertionError("A Summoner has Disconnected.\n(Assertion Error 'cause you instantiated class Ports.\nWhat part of private don't you understand, Nikhil...\nSigh.)\n");
+    }
     // PWM
-    private static final int INTAKE = 9;
-    private static final int CLIMBER = 8;
-    private static final int AGITATOR_LEFT = 6;
-    private static final int AGITATOR_RIGHT = 7;
+    public static final int INTAKE = 9;
+    public static final int CLIMBER = 8;
+    public static final int AGITATOR_LEFT = 6;
+    public static final int AGITATOR_RIGHT = 7;
+
+    // Drivetrain
+    public static final int DT_FRONT_LEFT = 1;
+    public static final int DT_BACK_LEFT = 2;
+    public static final int DT_FRONT_RIGHT = 3;
+    public static final int DT_BACK_RIGHT = 4;
 
     // Shooter
-    private static final int SERVO_LEFT_1 = 0;
-    private static final int SERVO_LEFT_2 = 1;
-    private static final int SERVO_RIGHT_1 = 2;
-    private static final int SERVO_RIGHT_2 = 3;
+    public static final int SERVO_LEFT_1 = 0;
+    public static final int SERVO_LEFT_2 = 1;
+    public static final int SERVO_RIGHT_1 = 2;
+    public static final int SERVO_RIGHT_2 = 3;
+
+    // Sensors
+    public static final int IR_CHANNEL = 2;
+    public static final int US_CHANNEL = 0;
 }
