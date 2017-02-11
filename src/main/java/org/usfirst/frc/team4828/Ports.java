af5422e425a752383e8ea158d688442fab50f18f
Jackie Chen <cheezballz3.14@gmail.com>
2017-02-11 12:12:53 -0500

    Add Ports.java

---
+++ src/main/java/org/usfirst/frc/team4828/Ports.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team4828;
+
+/**
+ * Created by Jackie on 2/11/2017.
+ */
+public class Ports {
+    // PWM
+    private static final int INTAKE = 9;
+    private static final int CLIMBER = 8;
+    private static final int AGITATOR_LEFT = 6;
+    private static final int AGITATOR_RIGHT = 7;
+
+    // Shooter
+    private static final int SERVO_LEFT_1 = 0;
+    private static final int SERVO_LEFT_2 = 1;
+    private static final int SERVO_RIGHT_1 = 2;
+    private static final int SERVO_RIGHT_2 = 3;
+}
