eb00a66e99f4548d150d1eb55980bfdb2a41d0b0
Joseph Ryan <josephryan3.14@gmail.com>
2017-02-13 18:20:50 -0500

    put overloaded functions next to eachother

--- src/main/java/org/usfirst/frc/team4828/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team4828/DriveTrain.java
@@ -110,6 +110,21 @@ public class DriveTrain {
     }
 
     /**
+     * Turn all wheels at set speeds
+     *
+     * @param fl Speed for Front Left Wheel
+     * @param fr Speed for Front Right Wheel
+     * @param bl Speed for Back Left Wheel
+     * @param br Speed for Back Right Wheel
+     */
+    public void testMotors(int fl, int fr, int bl, int br) {
+        frontLeft.set(fl);
+        frontRight.set(fr);
+        backLeft.set(bl);
+        backRight.set(br);
+    }
+
+    /**
      * Use PID to lock the robot in its current position
      */
     public void lock(){
@@ -132,20 +147,4 @@ public class DriveTrain {
         backRight.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
         backLeft.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
     }
-     
-    /**
-     * Turn all wheels at set speeds
-     *
-     * @param fl Speed for Front Left Wheel
-     * @param fr Speed for Front Right Wheel
-     * @param bl Speed for Back Left Wheel
-     * @param br Speed for Back Right Wheel
-     */
-    public void testMotors(int fl, int fr, int bl, int br) {
-        frontLeft.set(fl);
-        frontRight.set(fr);
-        backLeft.set(bl);
-        backRight.set(br);
-    }
-
 }
