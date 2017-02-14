5a133c2e4568b6654e440bd38bfb05e7236bcaa7
Joseph Ryan <josephryan3.14@gmail.com>
2017-02-13 19:03:05 -0500

    fix codestyle and naming

--- src/main/java/org/usfirst/frc/team4828/Hopper.java
+++ src/main/java/org/usfirst/frc/team4828/Hopper.java
@@ -7,21 +7,24 @@ public class Hopper {
     private static final double AGITATOR_SPEED = 0.5;
     private static final double INTAKE_SPEED = 0.5;
 
-    private Spark left, right, intakeMotor;
+    private Spark leftMotor;
+    private Spark rightMotor;
+    private Spark intakeMotor;
 
-    public void Hopper(int leftPort, int rightPort){
-        left = new Spark(leftPort);
-        right = new Spark(rightPort);
+    public Hopper(int leftPort, int rightPort, int intakePort){
+        leftMotor = new Spark(leftPort);
+        rightMotor = new Spark(rightPort);
+        intakeMotor = new Spark(intakePort);
     }
 
     public void stir(){
-        left.set(AGITATOR_SPEED);
-        right.set(AGITATOR_SPEED);
+        leftMotor.set(AGITATOR_SPEED);
+        rightMotor.set(AGITATOR_SPEED);
     }
 
     public void stopStir(){
-        left.set(0);
-        right.set(0);
+        leftMotor.set(0);
+        rightMotor.set(0);
     }
 
     public void intake(){
