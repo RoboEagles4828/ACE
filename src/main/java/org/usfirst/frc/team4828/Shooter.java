0213586cbee341bf01995314070ca4bfb6f35f5c
Joseph Ryan <josephryan3.14@gmail.com>
2017-02-21 15:42:55 -0500

    Remove indexer servo

--- src/main/java/org/usfirst/frc/team4828/Shooter.java
+++ src/main/java/org/usfirst/frc/team4828/Shooter.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj.Servo;
 
 public class Shooter extends Thread {
     private CANTalon shooterMotor;
-    private Servo indexer;
     public ServoGroup servos;
 
     private static final double SHOOTER_SPEED = 0.8;
@@ -15,34 +14,16 @@ public class Shooter extends Thread {
     private static final double I = 0;
     private static final double D = 0;
 
-    private double indexerOpen = .8;
-    private double indexerClose = .2;
-
     /**
      * Create shooter object that encapsulates all associated servos and motors.
      *
      * @param motorPort port of main shooter wheel
-     * @param indexerPort port of indexer servo
      * @param masterPort port of master angle control servo
      * @param slavePort port of slave angle control servo
      */
-    public Shooter(int motorPort, int indexerPort, int masterPort, int slavePort) {
+    public Shooter(int motorPort, int masterPort, int slavePort) {
         shooterMotor = new CANTalon(motorPort);
-        shooterMotor.changeControlMode(CANTalon.TalonControlMode.Speed);
-        shooterMotor.setPID(P, I, D);
-        indexer = new Servo(indexerPort);
         servos = new ServoGroup(masterPort, slavePort);
-    }
-
-    /**
-     * Calibrates the indexer
-     *
-     * @param open double 0-1 open position
-     * @param close double 0-1 close position
-     */
-    public void calibrateIndexer(double open, double close){
-        indexerOpen = open;
-        indexerClose = close;
     }
 
     public void spinUp() {
@@ -58,25 +39,15 @@ public class Shooter extends Thread {
         shooterMotor.set(speed * (1 - servos.get() * SERVO_MULTIPLIER));
     }
 
+    public void spinUpAbsolute(double speed){
+        shooterMotor.set(speed);
+    }
+
     /**
      * Slow the shooter wheel to a halt.
      */
     public void spinDown() {
         shooterMotor.set(0);
-    }
-
-    /**
-     * Open indexer servo to allow fuel to contact the shooter wheel
-     */
-    public void startShooter(){
-        indexer.set(indexerOpen);
-    }
-
-    /**
-     * Close indexer servo to block fuel from contacting the shooter wheel
-     */
-    public void stopShooter(){
-        indexer.set(indexerClose);
     }
 
     /**
