ea82898f07852284a168e8191a4fae361f7581bd
Joseph Ryan <josephryan3.14@gmail.com>
2017-02-21 15:34:14 -0500

    Integrate components
    Hopper + Shooter

--- src/main/java/org/usfirst/frc/team4828/Robot.java
+++ src/main/java/org/usfirst/frc/team4828/Robot.java
@@ -8,31 +8,42 @@ public class Robot extends IterativeRobot {
     private Joystick driveStick;
     private DriveTrain drive;
     private AHRS navx;
-    private DigitalInput ir;
-    private Shooter shoot;
+    private Shooter rightShooter, leftShooter;
     private Vision vision;
     private DigitalInput[] dipSwitch;
     private int autonSelect;
     private Climber climb;
+    private Hopper hopper;
 
     @Override
     public void robotInit() {
         super.robotInit();
         System.out.println("THE ROBOT TURNED ON");
         driveStick = new Joystick(0);
-        /*drive = new DriveTrain(
+        drive = new DriveTrain(
                 Ports.DT_FRONT_LEFT,
                 Ports.DT_BACK_LEFT,
                 Ports.DT_FRONT_RIGHT,
                 Ports.DT_BACK_RIGHT
-        );*/
+        );
         navx = new AHRS(SPI.Port.kMXP);
-        climb = new Climber(7, 8);
-        navx = new AHRS(SPI.Port.kMXP);
-        shoot = new Shooter(Ports.MOTOR_LEFT, Ports.INDEXER_LEFT, Ports.SERVO_LEFT_MASTER, Ports.SERVO_LEFT_SLAVE);
+        climb = new Climber(Ports.CLIMBER_1, Ports.CLIMBER_2);
+        hopper = new Hopper(Ports.AGITATOR, Ports.INTAKE);
+        if (vision != null) {
+            vision.terminate();
+        }
+        vision = new Vision(Ports.US_CHANNEL);
         // Master is the one on the right if you are looking at the back of the shooter
-        shoot.servos.calibrate(1, .3, 0);
-        shoot.servos.calibrate(2, .6, 1);
+        rightShooter = new Shooter(Ports.MOTOR_RIGHT, Ports.SERVO_RIGHT_MASTER, Ports.SERVO_RIGHT_SLAVE);
+        leftShooter = new Shooter(Ports.MOTOR_LEFT, Ports.SERVO_LEFT_MASTER, Ports.SERVO_LEFT_SLAVE);
+        rightShooter.servos.calibrate(1, .3, 0);
+        rightShooter.servos.calibrate(2, .6, 1);
+        leftShooter.servos.calibrate(1, .75, .35);
+        leftShooter.servos.calibrate(2, .3, .75);
+
+        // Start pointing straight up
+        rightShooter.servos.set(0);
+        leftShooter.servos.set(0);
 
         dipSwitch = new DigitalInput[4];
         dipSwitch[0] = new DigitalInput(Ports.DIPSWITCH_1);
@@ -44,6 +55,7 @@ public class Robot extends IterativeRobot {
     @Override
     public void autonomousInit() {
         super.autonomousInit();
+        // Bit shift the switches repeatedly to read it into an int
         for (int i = 0; i < 4; i++) {
             autonSelect += (dipSwitch[i].get() ? 1 : 0) * (1 << i);
         }
@@ -91,54 +103,22 @@ public class Robot extends IterativeRobot {
         if (driveStick.getRawButton(11)) {
             navx.reset();
         }
-        //System.out.println("Angle: " + navx.getAngle());
+        System.out.println("Angle: " + navx.getAngle());
     }
 
     @Override
     public void testInit() {
         super.testInit();
         System.out.println("Entering test...");
-        shoot.servos.set(.5);
-        vision = new Vision(Ports.US_CHANNEL);
-        vision.start();
     }
 
     @Override
     public void testPeriodic() {
-//        System.out.println("Ultrasonic Dist: " + us.distIn + " inches");
-//        Timer.delay(0.1);
-//        drive.mecanumDrive(driveStick.getX(), driveStick.getY(), driveStick.getTwist());
-//        Timer.delay(0.1);
-        drive.moveDistance(1000);
-        Timer.delay(1);
-        drive.moveDistance(-1000);
-        Timer.delay(1);
-//         if (driveStick.getRawButton(9)) {
-//             System.out.println("RAISING");
-//             shoot.servos.raise();
-//         }
-//         if (driveStick.getRawButton(10)) { //slave  = back left .66 - 1  master .33 - 0
-//             System.out.println("LOWERING");
-//             shoot.servos.lower();
-//         }
-//         if (driveStick.getRawButton(11)) {
-//             shoot.servos.set(0);
-//         }
-//         if (driveStick.getRawButton(12)) {
-//             shoot.servos.set(1);
-//         }
-//         System.out.println(shoot.servos);
-//         System.out.println(vision);
-//         System.out.println("move: " + vision.findX());
-//         Timer.delay(0.1);
+        Timer.delay(.1);
     }
 
     @Override
     public void disabledInit() {
         System.out.println("Disabling robot");
-        if (vision != null) {
-            vision.terminate();
-        }
-        System.out.println("Stopping thread");
     }
 }
